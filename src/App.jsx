510f456650959b5407a089faa9c46dc5b3a23983
Praveen Sinnur <praveensinnur@Praveens-MacBook-Air.local>
2025-10-12 11:17:04 +0530

    constants defined and navbar basics done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-indigo-700">
-    Hello world!
-  </h1>
-  )
-}
+    <main>
+      <NavBar />
+    </main>
+  );
+};
 
-export default App
+export default App;
