54c52e8f6787733ddf752d0cc5acd26b0429f961
Praveen Sinnur <praveensinnur@Praveens-MacBook-Air.local>
2025-10-14 20:41:16 +0530

    color change done for all models

--- src/components/models/Macbook-16.jsx
+++ src/components/models/Macbook-16.jsx
@@ -8,13 +8,33 @@ Source: https://sketchfab.com/3d-models/macbook-pro-m3-16-inch-2024-8e34fc2b3031
 Title: macbook pro M3 16 inch 2024
 */
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useGLTF, useTexture } from "@react-three/drei";
 
+import useMacbookStore from "../../store";
+
+import { noChangeParts } from "../../constants/index";
+import { Color } from "three";
+
 export default function MacbookModel16(props) {
-  const { nodes, materials } = useGLTF("/models/macbook-16-transformed.glb");
+  const { nodes, materials, scene } = useGLTF(
+    "/models/macbook-16-transformed.glb"
+  );
+
+  const { color } = useMacbookStore();
 
   const texture = useTexture("/screen.png");
+
+  useEffect(() => {
+    scene.traverse((child) => {
+      if (child.isMesh) {
+        if (!noChangeParts.includes(child.name)) {
+          child.material.color = new Color(color);
+        }
+      }
+    });
+  }, [color]);
+
   return (
     <group {...props} dispose={null}>
       <mesh
